e5163f922dfb101f9490469dc633e5b4bf228173
road3144 <fnfnfn258@gmail.com>
2022-06-30 10:42:36 +0900

    BOJ Tiny Memory Sort_10989

--- sort/bj_simpleSort_2750.py
+++ sort/bj_simpleSort_2750.py
@@ -2,9 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-data = []
+data = [0] * 10001
 for _ in range(n):
-    data.append(int(input()))
-data.sort()
-for i in data:
-    print(i)
+    data[int(input())] += 1
+for i in range(10001):
+    if data[i] != 0:
+        for _ in range(data[i]):
+            print(i)
